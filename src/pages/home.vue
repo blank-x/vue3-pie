0e6215630f9108b679a0c97eaec73496a403454d
zhaoshaoyong1970 <zhaoshaoyong1970@foxmail.com>
2021-05-08 18:39:18 +0800

    增加路由

---
+++ src/pages/home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>文字处理</p>
+    <div>禁止复制</div>
+    <div class="forbid-copy" @contextmenu="forbidContentMenu">
+      禁止复制禁止复制禁止复制禁止复制禁止复制禁止复制禁止复制禁止复制
+<!-- todo 行内url处理  ~assets/logo.png   @/assets/logo.png  不能生效； vue.config.js 好像没有被用到    -->
+      <img src="src/assets/logo.png" alt="" style="width: 50px" class="img-forbid-copy">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "home",
+  methods:{
+    forbidContentMenu(e){
+      e.preventDefault()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.forbid-copy{
+  /*禁止选中内容*/
+  user-select:none;
+  /*无法选中*/
+  /**/
+}
+.img-forbid-copy{
+  pointer-events:none;
+}
+</style>
---
+++ src/pages/juzhong.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+
+</div>
+</template>
+
+<script>
+export default {
+name: "juzhong"
+}
+
+</script>
+
+<style scoped>
+
+</style>
